b12669f7295b6c2415756eaa5eb6c62145088782
linuxdemon <walter@walterbarnes.net>
2016-06-05 05:56:45 -0500

    Update tumblr_block.js

--- tumblr_block.js
+++ tumblr_block.js
@@ -1,20 +1,34 @@
 var blogs = prompt("Blogs to block");
 var arr = blogs.split(" ");
-arr.forEach(function(element) {
-  jQuery.ajax({
-    url: '/svc/block/add',
-    type: 'post',
-    async: false,
-    data: {
-        tumblelog: $("div[class='navigation_inner'] h3 a span").innerHTML,
-        blocked_tumblelog: element
-    },
-    headers: {
-        'x-tumblr-form-key': $("meta[name='tumblr-form-key']").getAttribute('content')
-    },
-    error: function(xhr, status, err) 
+count = 0;
+
+arr.forEach(function (element) {
+    jQuery.ajax({
+        url: '/svc/block/add',
+        type: 'post',
+        beforeSend: function () {
+            count++;
+        },
+        data: {
+            tumblelog: $("div[class='navigation_inner'] h3 a.blog_name span.name").innerHTML,
+            blocked_tumblelog: element
+        },
+        headers: {
+            'X-tumblr-form-key': $("meta[name='tumblr-form-key']").getAttribute('content')
+        },
+        error: function (xhr, status, err) {
+            alert("Error occurred, please report to https://github.com/linuxdemon1/TumblrMassBlock/issues (please include the following data and any output from the console) \n Data: " + status + "\n" + err)
+        },
+        complete: function () {
+            count--;
+        }
+    });
+});
+
+int = setInterval(function () {
+    if (count < 1)
     {
-      alert("Error occured, please report to https://github.com/linuxdemon1/TumblrMassBlock/issues \n Data: " + status)
+        clearInterval(int);
+        alert("All blogs blocked, you may now refresh the page");
     }
-  });
-})
+}, 50);
